cee52ce825cad0228bf2bf3af29e8182f85cf722
Michelle Estefania Paredes Almachi <93295359+MichelleParedes@users.noreply.github.com>
2023-12-13 12:04:16 -0500

    Add files via upload

---
+++ Examen_1P/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int edad = 20;
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("La persona es mayor de edad.");
+        }
+        else
+        {
+            Console.WriteLine("La persona es menor de edad.");
+        }
+    }
+}
