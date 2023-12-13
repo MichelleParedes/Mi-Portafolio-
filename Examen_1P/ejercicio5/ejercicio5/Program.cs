36cdf97556b055035f9512f8c4a48472f5a5e68f
Michelle Estefania Paredes Almachi <93295359+MichelleParedes@users.noreply.github.com>
2023-12-13 12:08:02 -0500

    Add files via upload

---
+++ Examen_1P/ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Estudiante
+{
+    public string Nombre;
+    public int Edad;
+    public double Calificacion;
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Edad: {Edad}");
+        Console.WriteLine($"Calificación: {Calificacion}");
+    }
+}
+
+class EstudianteGraduado : Estudiante 
+{
+    public string Titulo; 
+}
+
+class Program
+{
+    static void Main()
+    {
+        EstudianteGraduado graduado1 = new EstudianteGraduado(); 
+        graduado1.Nombre = "María";
+        graduado1.Edad = 25;
+        graduado1.Calificacion = 9.2;
+        graduado1.Titulo = "Ingeniería Informática"; 
+
+        graduado1.MostrarInformacion(); 
+        Console.WriteLine($"Título obtenido: {graduado1.Titulo}"); 
+    }
+}
