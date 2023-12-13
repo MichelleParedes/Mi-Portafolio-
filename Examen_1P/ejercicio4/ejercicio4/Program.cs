f96c37424e2b9a072dc925b670e88991fb8254d8
Michelle Estefania Paredes Almachi <93295359+MichelleParedes@users.noreply.github.com>
2023-12-13 12:05:54 -0500

    Add files via upload

---
+++ Examen_1P/ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Estudiante
+{
+    public string Nombre;
+    public int Edad;
+    public double Calificacion;
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad: " + Edad);
+        Console.WriteLine("Calificación: " + Calificacion);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.Nombre = "Juan";
+        estudiante1.Edad = 20;
+        estudiante1.Calificacion = 8.5;
+
+        estudiante1.MostrarInformacion();
+    }
+}
