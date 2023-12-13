c83a8cdf112eb7e64e224f443ef21c844f9935ab
Michelle Estefania Paredes Almachi <93295359+MichelleParedes@users.noreply.github.com>
2023-12-13 12:02:24 -0500

    Add files via upload

---
+++ Examen_1P/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero1 = 10;
+        int numero2 = 5;
+        int resultado = numero1 + numero2;
+
+        Console.WriteLine("El resultado de la suma es: " + resultado);
+    }
+}
